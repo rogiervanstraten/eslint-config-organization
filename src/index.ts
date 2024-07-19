2b1ba76884c130dbd32d108354d3fec9ae247504
Rogier van Straten <rogier@from89.com>
2024-07-19 23:36:09 +0200

    feat: first prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Linter } from 'eslint';
 
 const config: Linter.Config = {
-	extends: 'airbnb-base',
-	rules: {
-		'no-console': ['warn'],
-		indent: ['error', 4],
-		'no-nested-ternary': ['error'],
-	},
+    extends: 'airbnb-base',
+    rules: {
+        'no-console': ['warn'],
+        indent: ['error', 4],
+        'no-nested-ternary': ['error'],
+    },
 };
 
 export = config;
